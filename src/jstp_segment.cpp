d0eec064f999614d35a073e69d2fa1329d41b259
Jack Smith <smit2395@git.cse.msu.edu>
2017-04-14 16:31:52 -0400

    Put skeleton in place for implimentation of the segment object

--- src/jstp_segment.cpp
+++ src/jstp_segment.cpp
@@ -1,1 +1,107 @@
 //Implimentation of jstp_segment.hpp
+
+#include "jstp_segment.hpp"
+
+vector<uint8_t> jstp_segment::serialize(){
+
+}
+
+void jstp_segment::deserialize(const vector<uint8_t>&){
+
+}
+
+uint32_t jstp_segment::get_sequence(){
+
+}
+
+uint32_t jstp_segment::get_ack(){
+
+}
+
+uint32_t jstp_segment::get_window(){
+
+}
+
+uint32_t jstp_segment::get_length(){
+
+}
+
+uint16_t jstp_segment::get_port(){
+
+}
+
+bool jstp_segment::get_syn_flag(){
+
+}
+
+bool jstp_segment::get_ack_flag(){
+
+}
+
+bool jstp_segment::get_close_flag(){
+
+}
+
+void jstp_segment::set_syn_flag(){
+
+}
+
+void jstp_segment::set_ack_flag(){
+
+}
+
+void jstp_segment::set_close_flag(){
+
+}
+
+void jstp_segment::reset_syn_flag(){
+
+}
+
+void jstp_segment::reset_ack_flag(){
+
+}
+
+void jstp_segment::reset_close_flag(){
+
+}
+
+void jstp_segment::set_sequence(uint32_t){
+
+}
+
+void jstp_segment::set_ack(uint32_t){
+
+}
+
+void jstp_segment::set_window(uint32_t){
+
+}
+
+void jstp_segment::set_port(uint16_t){
+
+}
+
+void jstp_segment::clear_payload(){
+
+}
+
+void jstp_segment::set_payload(vector<uint8_t>){
+
+}
+
+vector<uint8_t>::iterator jstp_segment::payload_begin(){
+
+}
+
+vector<uint8_t>::iterator jstp_segment::payload_end(){
+
+}
+
+//Private data members I can use
+/* uint32_t sequence; */
+/* uint32_t ack; */
+/* uint32_t window; */
+/* bitset<16> flags; */
+/* uint16_t port; */
+/* vector<uint8_t> payload; */
