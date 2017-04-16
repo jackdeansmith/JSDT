61f12b5ed3c5517deada937cc6826ae1a949a4cd
Jack Smith <smit2395@git.cse.msu.edu>
2017-04-16 15:07:20 -0400

    Constructors for the connector and acceptor

--- src/jstp_streams.cpp
+++ src/jstp_streams.cpp
@@ -1,1 +1,21 @@
 /* Implementation of the classes defined in jstp_streams.hpp */
+
+#include "jstp_streams.hpp"
+#include "jstp_segment.hpp"
+
+#include <string>
+using std::string;
+
+#include <cstdint>
+
+//Constructor, only thing we need to do for the connector class
+jstp_connector::jstp_connector(string h, uint16_t p): hostname(h), port(p){}
+
+//Constructor for the jstp acceptor, also the only thing we need to do for this
+//class, the default destructor should do just fine.
+jstp_acceptor::jstp_acceptor(uint16_t portno):
+    acceptor_socket(jstp_segment::MAX_SEGMENT_SIZE){
+    
+    //Bind the socket to the specified port
+    acceptor_socket.bind_local(portno);
+}
